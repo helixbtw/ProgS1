a4105958d17bf82161e724a1a32df84b67466788
helixbtw <48182352+helixbtw@users.noreply.github.com>
2022-09-20 17:47:23 +0200

    Add files via upload

---
+++ 1.1/1.1/Pole prostokąta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int pole;
+            Console.WriteLine("Bok a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bok b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            pole = a * b;
+            Console.WriteLine("Pole:" + pole);
+            Console.ReadKey(true);
+        }
+    }
+}
+
